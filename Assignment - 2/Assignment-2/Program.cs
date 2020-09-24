16b4f9e2895f42e118e5a3fdb8daa4e8e2f4215c
Cosmic Koirala <koiralacosmic@gmail.com>
2020-09-24 08:45:50 +0545

    Assignment Completed

--- Assignment - 2/Assignment-2/Program.cs
+++ Assignment - 2/Assignment-2/Program.cs
@@ -6,7 +6,8 @@ namespace Assignment_2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Directories directories = new Directories();
+            directories.CreateDirectories();
         }
     }
 }
